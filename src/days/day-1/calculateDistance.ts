74d3f01ea8f9416eeb66e6976efe68415d64d5bb
LZygmunt <lzygmunt0@gmail.com>
2024-12-10 22:36:44 +0100

    feat: solve first part of day 1

---
+++ src/days/day-1/calculateDistance.ts
@@ -0,0 +1,11 @@
+import { open } from 'node:fs/promises';
+import * as path from 'node:path';
+
+export const calculateDistance = (leftList: number[], rightList: number[]) => {
+  const sortedLeftList = leftList.toSorted((a, b) => a - b)
+  const sortedRightList = rightList.toSorted((a, b) => a - b)
+
+  return sortedLeftList
+    .reduce((acc, cur, ind) => Math.abs(cur - sortedRightList[ind]) + acc, 0);
+}
+
