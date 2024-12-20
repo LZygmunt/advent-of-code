25d3b5ed6834661a4d0b7ac2e1973fe72d2c13bc
LZygmunt <lzygmunt@gmail.com>
2024-12-20 23:36:44 +0100

    feat(day-2): implement counting safe reports

--- src/days/day-2/countSafeReports.ts
+++ src/days/day-2/countSafeReports.ts
@@ -1,6 +1,14 @@
 import type { List } from './types.ts';
+import isReportSafe from './isReportSafe.ts';
 
 type CountSafeReports = ( reports: List[] ) => number;
-const countSafeReports: CountSafeReports = ( reports ) => 0;
+
+const countSafeReports: CountSafeReports = ( reports ) =>
+  reports.reduce(
+    ( totalSafeReports, report ) => isReportSafe( report )
+      ? totalSafeReports + 1
+      : totalSafeReports,
+    0,
+  );
 
 export default countSafeReports;
