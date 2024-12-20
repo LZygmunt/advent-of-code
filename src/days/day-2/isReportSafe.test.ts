2262b3dcce1781da97a6fb4cbdf9d0d0ad1a9968
LZygmunt <lzygmunt0@gmail.com>
2024-12-20 23:21:26 +0100

    test(day-2): adjust test cases

--- src/days/day-2/isReportSafe.test.ts
+++ src/days/day-2/isReportSafe.test.ts
@@ -3,13 +3,18 @@ import isReportSafe from './isReportSafe.ts';
 it.each`
 input                  | expected
 ${ [ 7, 6, 4, 2, 1 ] } | ${ true }
+${ [ 1, 3, 6, 7, 9 ] } | ${ true }
+`( 'given report $input is safe', ( { input, expected } ) => {
+  expect( isReportSafe( input ) ).toBe( expected );
+} );
+
+it.each`
+input                  | expected
 ${ [ 1, 2, 7, 8, 9 ] } | ${ false }
 ${ [ 9, 7, 6, 2, 1 ] } | ${ false }
 ${ [ 1, 3, 2, 4, 5 ] } | ${ false }
 ${ [ 8, 6, 4, 4, 1 ] } | ${ false }
-${ [ 1, 3, 6, 7, 9 ] } | ${ true }
 ${ [ 3, 1, 4, 7, 9 ] } | ${ false }
-`// @ts-ignore
-  ( `$input is ${'$expected' === 'true' ? '': 'un'}safe`, ( { input, expected } ) => {
-    expect( isReportSafe( input ) ).toBe( expected );
-  } );
+`( 'given report $input is unsafe', ( { input, expected } ) => {
+  expect( isReportSafe( input ) ).toBe( expected );
+} );
