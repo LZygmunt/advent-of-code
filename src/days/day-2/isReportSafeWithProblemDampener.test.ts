ae450cbd37c24a4cb002d677e60e3fad076da186
LZygmunt <lzygmunt@gmail.com>
2024-12-21 14:30:21 +0100

    feat(day-2): solve problem Dampener

--- src/days/day-2/isReportSafeWithProblemDampener.test.ts
+++ src/days/day-2/isReportSafeWithProblemDampener.test.ts
@@ -7,6 +7,10 @@ ${ [ 1, 3, 2, 4, 5 ] } | ${ true }
 ${ [ 8, 6, 4, 4, 1 ] } | ${ true }
 ${ [ 1, 3, 6, 7, 9 ] } | ${ true }
 ${ [ 12, 16, 17, 18, 21, 24, 27, 30 ] } | ${ true }
+${ [ 3, 1, 4, 7, 9 ] } | ${ true }
+${ [ 3, 4, 1, 7, 9 ] } | ${ true }
+${ [ 3, 4, 7, 1, 9 ] } | ${ true }
+${ [ 3, 4, 7, 9, 1 ] } | ${ true }
 `( 'given report $input is safe', ( { input, expected } ) => {
   expect( isReportSafeWithProblemDampener( input ) ).toBe( expected );
 } );
@@ -15,7 +19,6 @@ it.each`
 input                  | expected
 ${ [ 1, 2, 7, 8, 9 ] } | ${ false }
 ${ [ 9, 7, 6, 2, 1 ] } | ${ false }
-${ [ 3, 1, 4, 7, 9 ] } | ${ false }
 `( 'given report $input is unsafe', ( { input, expected } ) => {
   expect( isReportSafeWithProblemDampener( input ) ).toBe( expected );
 } );
