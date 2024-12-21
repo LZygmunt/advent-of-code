7757b80742a1785791fb93f6d8c8de116d0908f2
LZygmunt <lzygmunt0@gmail.com>
2024-12-21 14:30:21 +0100

    feat(day-2): solve problem Dampener

--- src/days/day-2/isReportSafeWithProblemDampener.ts
+++ src/days/day-2/isReportSafeWithProblemDampener.ts
@@ -1,8 +1,20 @@
 import type { ReportSafeChecker } from './types.ts';
+import isReportSafe from './isReportSafe.ts';
 
 const isReportSafeWithProblemDampener: ReportSafeChecker = ( list ) => {
+  if ( isReportSafe( list ) ) {
+    return true;
+  }
+  let probablySafeReports = [];
+  for ( let i = 0; i <= list.length - 1; i++ ) {
+    probablySafeReports = list.toSpliced( i, 1 );
 
-  return true;
+    if ( isReportSafe( probablySafeReports ) ) {
+      return true;
+    }
+  }
+
+  return false;
 };
 
 export default isReportSafeWithProblemDampener;
